42ba6f44afc9dc4c67b25cfcccba0076d249b004
ayala-usma <ayala.usma@gmail.com>
2017-11-28 16:16:44 -0500

    Changes to visualization

--- red_contratistas/js/red_contratistas.js
+++ red_contratistas/js/red_contratistas.js
@@ -31,9 +31,9 @@ height = height - margin.top - margin.bottom;
 //Creation of the simulation parameters: Creation of the forces that will mandate the simulation
 var forceSimulation = d3.forceSimulation()
 						.force("collide", d3.forceCollide().radius(function(d) {return 5})) //Prevents nodes from overlapping
-						.force("radial", d3.forceRadial(function(d) { return (d.group == "entidad") ? -30 : 200; })//.y(height/2).x(width/2)) //Sends contratistas to the outside
+						.force("radial", d3.forceRadial(function(d) { return (d.group == "entidad") ? -30 : 300; }).y(height/2).x(width/2)) //Sends contratistas to the outside
 						.force("link", d3.forceLink().id(function(d) { return (d.id) })) //Provides link forces to the nodes connected between them
-						.force("center", d3.forceCenter((width / 2), (height / 2)));
+            .force("center", d3.forceCenter((width / 2), (height / 2)));
 
 
 //Read the JSON formatted data
@@ -65,10 +65,7 @@ d3.json("datasets/red_contratistas.json", function(error, data) {
   	  			  .call(d3.drag()
   	  			  	.on("start", dragstarted)
   	  			  	.on("drag", dragged)
-  	  			  	.on("end", dragended))
-//  	  			  .on("mouseover", mouseOver(.2))
-//  	  			  .on("mouseout", mouseOut);
-
+  	  			  	.on("end", dragended));
 
   	  drawingNodes.append("title")
   	  			  .text(function(d) { if(d.group == "contratista") {return "Nombre: " + d.name + "\n" + "Cuantía total acumulada devengada: " + numberFormat(d.cuantiaContratos) + "\n" + "Tipo: Contratista";} 
@@ -76,15 +73,10 @@ d3.json("datasets/red_contratistas.json", function(error, data) {
 
   //Carrying out the simulation
   forceSimulation.nodes(nodes).on("tick", ticked);
-//  forceSimulation.force("link").links(edges);
-
-
-  
 
   //Ticked function to establish the simulation behavior
 	function ticked() {
 		drawingNodes.attr("transform", positionNode);
-        drawingLinks.attr("d", positionLink);
 
 	}
   
@@ -106,26 +98,6 @@ d3.json("datasets/red_contratistas.json", function(error, data) {
         return "translate(" + d.x + "," + d.y + ")";
     }
 
-    //Function to draw links between entidades and contratistas
-    function positionLink(d) {
-        var offset = 30;
-
-        var midpoint_x = (d.source.x + d.target.x) / 2;
-        var midpoint_y = (d.source.y + d.target.y) / 2;
-
-        var dx = (d.target.x - d.source.x);
-        var dy = (d.target.y - d.source.y);
-
-        var normalise = Math.sqrt((dx * dx) + (dy * dy));
-
-        var offSetX = midpoint_x + offset*(dy/normalise);
-        var offSetY = midpoint_y - offset*(dx/normalise);
-
-        return "M" + d.source.x + "," + d.source.y +
-            "S" + offSetX + "," + offSetY +
-            " " + d.target.x + "," + d.target.y;
-    }
-
 
    //Functions that define the drag actions
       function dragstarted(d) {
@@ -144,60 +116,6 @@ d3.json("datasets/red_contratistas.json", function(error, data) {
       d.fx = null;
       d.fy = null;
     }
-
-   //Functions related to link placement
-    
-    //This function builds a dictionary of links and nodes
-    var linkedByIndex = {};
-    edges.forEach(function(d) {
-        linkedByIndex[d.source + "," + d.target] = 1;
-    });
-
-    // check the dictionary to see if nodes are linked
-    function isConnected(a, b) {
-        return linkedByIndex[a.id + "," + b.id] || linkedByIndex[b.id + "," + a.id] || a.id === b.id;
-    }
   
-  //Fits a linear width scale for the links on mouseover
-//  var min_width = 0.3,
-//      max_width = 5,  
-//      scale_converter = d3.scaleLinear().domain([d3.min(edges, function(d){return d.weight}),d3.max(edges, function(d){return d.weight})]).range([min_width,max_width]);
-  
-    // fade nodes on hover
-    function mouseOver(opacity) {
-        return function(d) {
-            // check all other nodes to see if they're connected
-            // to this one. if so, keep the opacity at 1, otherwise
-            // fade
-            drawingNode.style("stroke-opacity", function(o) { 
-              thisOpacity = isConnected(d, o) ? 1 : opacity;
-                return thisOpacity;
-            });
-            drawingNode.style("fill-opacity", function(o) {
-                thisOpacity = isConnected(d, o) ? 1 : opacity;
-                return thisOpacity;
-            });
-            // also style link accordingly
-            drawingLinks.style("stroke-opacity", function(o) {
-                return o.source === d || o.target === d ? 1 : opacity;
-            });
-           // drawingLinks.style("stroke", function(o){
-           //     return o.source === d || o.target === d ? color(proteinType.indexOf(o.source.type)) : "#ddd";
-           // });
-            //drawingLinks.style("stroke-width", function(o){return scale_converter(o.weight)});          
-        };
-    }
-
-    //Restore original colors and opacity after mouse out
-        function mouseOut() {
-            drawingNode.style("stroke-opacity", 1);
-            drawingNode.style("fill-opacity", 1);
-            drawingLinks.style("stroke-opacity", 1);
-            drawingLinks.style("stroke", "#ddd");
-          	drawingLinks.style("stroke-width", 1);
-        }
-      
-
-
 });
 
